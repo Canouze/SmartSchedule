71809a94685feb0d93d54ceca55d4a32afb6c598
Canouze <92824987+Canouze@users.noreply.github.com>
2022-12-09 00:59:06 +1100

    Ongoing changes to proto files, html, js etc and added php functionality

--- server/app.js
+++ server/app.js
@@ -1,14 +1,44 @@
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
-var PROTO_PATH = __dirname + "/protos/scheduler.proto"
+var PROTO_PATH = __dirname + "/protos/employee.proto"
 var packageDefinition = protoLoader.loadSync(
   PROTO_PATH
 )
-var proto = grpc.loadPackageDefinition(packageDefinition).package_name
+var employee_proto = grpc.loadPackageDefinition(packageDefinition).SmartSchedule;
+
+var template_employee = {
+  name: "John Doe",
+  employeeID: "0138",
+  startDate: "23/02/2015",
+  level: "3"
+}
+
+let employee_list = [template_employee];
+
+
+function getEmployees(call, callback){
+  var fil1 = parseInt(call.request.minDurationDays);
+  var fil2 = parseInt(call.request.minLevel);
+  var currDate = new Date();
+  for(let i=0; i<employee_list.length; i++){
+    var date2 = new Date(employee_list[i].startDate);
+    var duration = currDate-date2;
+    if(fil1<duration&&fil2<employee_list[i].level){
+      call.write({
+        emplyeeName: employee_list[j].name,
+        employeeID: employee_list[j].employeeID,
+        employeeStartDate: employee_list[j].startDate,
+        employeeLevel: employee_list[j].level
+      })
+    }
+  }
+  call.end();
+}
 
 
 
-var server = new grpc.Server()
-server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() {
+var server = new grpc.Server();
+server.addService(employee_proto.EmployeeService.service, {getEmployees: getEmployees});
+server.bindAsync("0.0.0.0:39237", grpc.ServerCredentials.createInsecure(), function() {
   server.start()
 })
--- web-client/routes/index.js
+++ web-client/routes/index.js
@@ -1,23 +1,37 @@
+var readlineSync = require('readline-sync')
 var express = require('express');
-
 var router = express.Router();
-
 var grpc = require('@grpc/grpc-js');
-
 var protoLoader = require('@grpc/proto-loader');
-
-var PROTO_PATH = __dirname + '/../protos/scheduler.proto';
-
+var PROTO_PATH = __dirname + '/../protos/employee.proto';
 var packageDefinition = protoLoader.loadSync(PROTO_PATH);
+var employee_proto = grpc.loadPackageDefinition(packageDefinition).SmartSchedule;
+var client = new employee_proto.EmployeeService('0.0.0.0:39237', grpc.credentials.createInsecure());
+let displaylist = [];
 
-var calc_proto = grpc.loadPackageDefinition(packageDefinition).scheduler;
-
-var client = new calc_proto.SchedulerService('0.0.0.0:39237', grpc.credentials.createInsecure());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   try{
-    res.render('scheduler', { title: 'Scheduler' });
+    res.render('index', {
+      title: 'SmartSchedule'
+    });
+  }
+  catch(error){
+    console.log(error);
+  }
+});
+
+router.get('/employee', function(req, res, next) {
+  try{
+    employeeRunner();
+    res.render('employee', {
+    title: 'Employees',
+    employeeName:'hhhh',
+    employeeID: '',
+    employeeStartDate: '',
+    employeeLevel: ''
+  });
   }
   catch(error){
     console.log(error);
@@ -25,3 +39,47 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+function employeeRunner(){
+  displaylist=[];
+  var initdays = "5000";
+  var initlevel = "1";
+  var temp_employee = {
+    name: "",
+    employeeID: "",
+    startDate: "",
+    level: ""
+  }
+  client.getEmployees({minDurationDays: initdays, minLevel: initlevel}, function (error, response){
+    call.on('data', function(request){
+      temp_employee.name = request.employeeName;
+      temp_employee.employeeID = request.employeeID;
+      temp_employee.startDate = request.employeeStartDate;
+      temp_employee.level = request.employeeLevel;
+      displaylist.push(temp_employee);
+    })
+    call.on('end', function(){
+      res.render('employee', {
+        title: 'Employees',
+        employeeName: displaylist[i].name,
+        employeeID: displaylist[i].employeeID,
+        employeestartDate: displaylist[i].startDate,
+        employeeLevel: displaylist[i].level,
+      });
+    })
+    call.on('error', function(e) {
+      console.log("An error occurred")
+    })
+  })
+}
+
+//this function updates price slider value
+function filterone(){
+	daysfilval.innerHTML=daysfil.value;
+}
+
+//this function updates calorie slider value
+function filtertwo(){
+	levelfilval.innerHTML=levelfil.value;
+}
