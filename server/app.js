4bd4d47aa658c64d8711056dee57cefaf1a97a8b
Canouze <92824987+Canouze@users.noreply.github.com>
2022-12-14 16:00:17 +1100

    Almost complete only issue with schedule to fix

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
-var PROTO_PATH = __dirname + "/protos/employee.proto"
-var packageDefinition = protoLoader.loadSync(PROTO_PATH)
-var employee_proto = grpc.loadPackageDefinition(packageDefinition).SmartSchedule;
+var PROTO_PATH_1 = __dirname + "/protos/employee.proto"
+var PROTO_PATH_2 = __dirname + "/protos/project.proto"
+var PROTO_PATH_3 = __dirname + "/protos/schedule.proto"
+var packageDefinition1 = protoLoader.loadSync(PROTO_PATH_1)
+var packageDefinition2 = protoLoader.loadSync(PROTO_PATH_2)
+var packageDefinition3 = protoLoader.loadSync(PROTO_PATH_3)
+var employee_proto = grpc.loadPackageDefinition(packageDefinition1).SmartSchedule;
+var project_proto = grpc.loadPackageDefinition(packageDefinition2).SmartScheduleProject;
+var schedule_proto = grpc.loadPackageDefinition(packageDefinition3).SmartScheduleMain;
 
 var mysql = require('mysql');
 
@@ -19,14 +25,25 @@ con.connect(function(err) {
 });
 
 let employee_list = [];
-
 let displaylist = [];
+
+let project_list=[];
+let pdisplaylist=[];
+
+let schedule_list=[];
 
 let placeholder = {
   employeeID: "No data",
   emplyeeName: "No data",
   emplyeeStartDate: "No data",
   emplyeeName: "No data",
+}
+
+let placeholder2 = {
+  projectID: "No data",
+  projectName: "No data",
+  clientName: "No data",
+  projectDeadline: "No data",
 }
 
 
@@ -95,11 +112,141 @@ function giveEmployee(call, callback){
   }
 }
 
+function getProject(call, callback){
+  try{
+    project_list=[];
+    pdisplaylist=[];
+    con.query("SELECT * FROM projects", function (err, result, fields) {
+      if (err) throw err;
+      for(let i=0; i<result.length; i++){
+        temp_project = {
+          projectID: result[i].project_id,
+          projectName: result[i].project_name,
+          clientName: result[i].client_name,
+          projectDeadline: result[i].deadline
+        };
+        project_list.push(temp_project);
+      }
+      var pfil1 = parseInt(call.request.daysToDeadline);
+      var currDate = new Date();
+      for(let i=0; i<project_list.length; i++){
+        var pdate = new Date(project_list[i].projectDeadline);
+        var pdur = (pdate.getTime()-currDate.getTime())/86400000;
+        if(pfil1>=pdur){
+          pdisplaylist.push(project_list[i]);
+        }
+      }
+      if(pdisplaylist.length===0){
+        pdisplaylist.push(placeholder2);
+        callback(null, {project: pdisplaylist, total: project_list.length, provided: 0})
+      }
+      else{
+        callback(null, {project: pdisplaylist, total: project_list.length, provided: pdisplaylist.length})
+      }
+    });
+  }
+  catch(e){
+    callback(null, {
+      message: "An error occurred"
+    })
+  }
+}
+
+function giveProject(call, callback){
+  try{
+    var received_id=call.request.project.projectID;
+    var received_name=call.request.project.projectName;
+    var received_startdate=call.request.project.clientName;
+    var received_level=call.request.project.projectDeadline;
+    var sql_instruct = "INSERT INTO projects (project_id, project_name, client_name, deadline) VALUES ('"+received_id+"', '"+received_name+"', '"+received_startdate+"', '"+received_level+"');";
+    con.query(sql_instruct, function (err, result) {
+      if (err) throw err;
+      console.log("Project record created successfully");
+      callback(null, {
+        employeeResult: "Project Created Successfully."
+      })
+    });
+  }
+  catch(e){
+    console.log(e);
+    callback(null, {
+      message: "An error occurred"
+    })
+  }
+}
+
+function getSchedule(call, callback){
+  try{
+    schedule_list=[];
+    con.query("SELECT * FROM schedule", function (err, result, fields) {
+      if (err) throw err;
+      for(let i=0; i<result.length; i++){
+        temp_schedule = {
+          scheduleID: result[i].schedule_id,
+          scheduleDay: result[i].schedule_day,
+          employeeName: result[i].employee_name,
+          projectName: result[i].project_name,
+        };
+        schedule_list.push(temp_schedule);
+      }
+      console.log(schedule_list);
+      callback(null, {schedule: schedule_list})
+    })
+  }
+  catch(e){
+    callback(null, {
+      message: "An error occurred"
+    })
+  }
+}
+
+function giveSchedule(call, callback){
+  try{
+    var received_sday=call.request.scheduleDay;
+    var received_empname=call.request.scheduleEmployee;
+    var received_projname=call.request.scheduleProject;
+    var sql_instruct = "INSERT INTO schedule (schedule_day, employee_name, project_name) VALUES ('"+received_sday+"', '"+received_empname+"', '"+received_projname+"');";
+    con.query(sql_instruct, function (err, result) {
+      if (err) throw err;
+      console.log("Schedule record created successfully");
+    });
+    con.query("SELECT * FROM schedule", function (err, result, fields) {
+      if (err) throw err;
+      for(let i=0; i<result.length; i++){
+        temp_schedule = {
+          scheduleID: result[i].schedule_id,
+          scheduleDay: result[i].schedule_day,
+          employeeName: result[i].employee_name,
+          projectName: result[i].project_name,
+        };
+        schedule_list.push(temp_schedule);
+      }
+      console.log(schedule_list);
+      callback(null, {schedule: schedule_list})
+    })
+  }
+  catch(e){
+    console.log(e);
+    callback(null, {
+      message: "An error occurred"
+    })
+  }
+}
+
 var server = new grpc.Server();
 server.addService(employee_proto.EmployeeService.service, {
   getEmployees: getEmployees,
   giveEmployee: giveEmployee
 });
+server.addService(project_proto.ProjectService.service, {
+  getProject: getProject,
+  giveProject: giveProject
+});
+server.addService(schedule_proto.ScheduleService.service, {
+  getSchedule: getSchedule,
+  giveSchedule: giveSchedule
+});
+
 server.bindAsync("0.0.0.0:39237", grpc.ServerCredentials.createInsecure(), function() {
   server.start()
 })
--- web-client/routes/index.js
+++ web-client/routes/index.js
@@ -2,13 +2,24 @@ var express = require('express');
 var router = express.Router();
 var grpc = require('@grpc/grpc-js');
 var protoLoader = require('@grpc/proto-loader');
-var PROTO_PATH = __dirname + '/../protos/employee.proto';
-var packageDefinition = protoLoader.loadSync(PROTO_PATH);
-var employee_proto = grpc.loadPackageDefinition(packageDefinition).SmartSchedule;
+var PROTO_PATH_1 = __dirname + '/../protos/employee.proto';
+var PROTO_PATH_2 = __dirname + '/../protos/project.proto';
+var PROTO_PATH_3 = __dirname + '/../protos/schedule.proto';
+var packageDefinition1 = protoLoader.loadSync(PROTO_PATH_1);
+var packageDefinition2 = protoLoader.loadSync(PROTO_PATH_2);
+var packageDefinition3 = protoLoader.loadSync(PROTO_PATH_3);
+var employee_proto = grpc.loadPackageDefinition(packageDefinition1).SmartSchedule;
+var project_proto = grpc.loadPackageDefinition(packageDefinition2).SmartScheduleProject;
+var schedule_proto = grpc.loadPackageDefinition(packageDefinition3).SmartScheduleMain;
 var client = new employee_proto.EmployeeService('0.0.0.0:39237', grpc.credentials.createInsecure());
+var projectClient = new project_proto.ProjectService('0.0.0.0:39237', grpc.credentials.createInsecure());
+var scheduleClient = new schedule_proto.ScheduleService('0.0.0.0:39237', grpc.credentials.createInsecure());
 
 let receivedlist = [];
-
+let preceivedlist=[];
+let showSchedule=[];
+let temps = [];
+let daylist = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,6 +30,109 @@ router.get('/', function(req, res, next) {
   }
   catch(error){
     console.log(error);
+  }
+});
+
+router.get('/schedule', function(req, res, next) {
+  try{
+    showSchedule = [];
+    var eres = [];
+    var pres = [];
+    var sres = [];
+    client.getEmployees({minDurationDays: 0, minLevel: 0}, function (error, response){
+      eres = response.employee;
+      //console.log(eres);
+      projectClient.getProject({daysToDeadline: 5000}, function (error, response){
+        pres = response.project;
+        scheduleClient.getSchedule(null, function(error, response){
+          sres = response.schedule;
+          for(let j=0; j<eres.length; j++){
+            temps=[];
+            //console.log(1);
+            temps.push(eres[j].employeeName);
+            for(let g=0; g<daylist.length; g++){
+              //console.log(2);
+              for(let k=0; k<sres.length; k++){
+                //console.log(3);
+                if(eres[j].employeeName===sres[k].employeeName&&sres[k].scheduleDay===daylist[g]){
+                  temps.push(sres[k].projectName);
+                }
+                else{
+                  temps.push("Available");
+                }
+              }
+            }
+            showSchedule.push(temps);
+          }
+          try{
+            res.render('schedule', {
+              title: 'Schedule',
+              pres: pres,
+              eres: eres,
+              sres: showSchedule
+            })
+          }
+          catch(e){
+            console.log(e);
+          }
+        })
+      })
+    })
+  }
+  catch(error){
+    console.log(error);
+  }
+});
+
+router.get('/make-schedule', function(req, res, next) {
+  if(req.query.selectproject!=undefined)
+    try{
+      var initselectemp = req.query.selectemployee;
+      var initselectproj = req.query.selectproject;
+      var initselectday = req.query.selectday;
+      scheduleClient.giveSchedule({scheduleDay: initselectday, scheduleEmployee: initselectemp,scheduleProject: initselectproj}, function (error, response){
+        eres = response.employee;
+        pres = response.project;
+        sres = response.schedule;
+        var eres = [];
+        var pres = [];
+        var sres = [];
+        showSchedule = [];
+        client.getEmployees({minDurationDays: 0, minLevel: 0}, function (error, response){
+          eres = response.employee;
+          projectClient.getProject({daysToDeadline: 5000}, function (error, response){
+            pres = response.project;
+            scheduleClient.getSchedule(null, function(error, response){
+              sres = response.schedule;
+              try{
+                res.render('schedule', {
+                  title: 'Schedule',
+                  pres: pres,
+                  eres: eres,
+                  sres: showSchedule
+                })
+              }
+              catch(e){
+                console.log(e);
+              }
+            })
+          })
+        })
+      })
+    }
+    catch(e){
+      console.log(e);
+    }
+  else{
+    try{
+      res.render('create-employee', {
+        title: 'Create Employee',
+        result: 'Please enter details...'
+      })
+    }
+    catch(e){
+      console.log(e);
+    }
   }
 });
 
@@ -33,7 +147,30 @@ router.get('/employee', function(req, res, next) {
           title: 'Employees',
           result: response.employee,
           shown: response.provided,
-          filtout: response.total-response.provided,
+          filtout: response.total-response.provided
+        })
+      }
+      catch(e){
+        console.log(error);
+      }
+    })
+  }
+  catch(error){
+    console.log(error);
+  }
+});
+
+router.get('/project', function(req, res, next) {
+  try{
+    pdisplaylist=[];
+    var initdead = req.query.deadValue || 600;
+    projectClient.getProject({daysToDeadline: initdead}, function (error, response){
+      try{
+        res.render('project', {
+          title: 'Projects',
+          result: response.project,
+          pshown: response.provided,
+          pfiltout: response.total-response.provided,
         })
       }
       catch(e){
@@ -73,6 +210,41 @@ router.get('/create-employee', function(req, res, next) {
     try{
       res.render('create-employee', {
         title: 'Create Employee',
+        result: 'Please enter details...'
+      })
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+});
+
+router.get('/create-project', function(req, res, next) {
+  if(req.query.projectid!=undefined)
+    try{
+      var initid = req.query.projectid;
+      var initname = req.query.projectname;
+      var initclient = req.query.clientname;
+      var initdeadline = req.query.projectdeadline;
+      projectClient.giveProject({project: {projectID: initid, projectName: initname, clientName: initclient, projectDeadline: initdeadline}}, function (error, response){
+        try{
+          res.render('create-project', {
+            title: 'Create Project',
+            result: response.projectResult
+          })
+        }
+        catch(e){
+          console.log(e);
+        }
+      })
+    }
+    catch(e){
+      console.log(e);
+    }
+  else{
+    try{
+      res.render('create-project', {
+        title: 'Create Project',
         result: 'Please enter details...'
       })
     }
